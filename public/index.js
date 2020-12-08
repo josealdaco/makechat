48fff92cc6c01fb0dc5c8d96d1db4e6f7d855262
siko408 <gamerbloxhd@gmail.com>
2020-12-08 09:40:38 -0800

    ---new channel created

--- public/index.js
+++ public/index.js
@@ -63,3 +63,13 @@ socket.on('new message', (data) => {
 })
 
 })
+
+$('#new-channel-btn').click( () => {
+  let newChannel = $('#new-channel-input').val();
+
+  if(newChannel.length > 0){
+    // Emit the new channel to the server
+    socket.emit('new channel', newChannel);
+    $('#new-channel-input').val("");
+  }
+})
