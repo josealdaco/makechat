197f81b04578c58edab2fd144c997e950fc4b6c7
siko408 <gamerbloxhd@gmail.com>
2020-12-08 09:32:29 -0800

    ---connected all sockets

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 //index.js
 $(document).ready(()=>{
-  const socket = io.connect();
-
-  //Keep track of the current user
+    const socket = io.connect();
   let currentUser;
+  // Get the online users from the server
+  socket.emit('get online users');
 
   $('#create-user-btn').click((e)=>{
     e.preventDefault();
@@ -36,6 +36,21 @@ $(document).ready(()=>{
     console.log(`${username} has joined the chat`);
     $('.users-online').append(`<div class="user-online">${username}</div>`);
   })
+  socket.on('get online users', (onlineUsers) => {
+    //You may have not have seen this for loop before. It's syntax is for(key in obj)
+    //Our usernames are keys in the object of onlineUsers.
+    for(username in onlineUsers){
+      $('.users-online').append(`<div class="user-online">${username}</div>`);
+    }
+  })
+
+  //Refresh the online user list
+  socket.on('user has left', (onlineUsers) => {
+    $('.users-online').empty();
+    for(username in onlineUsers){
+      $('.users-online').append(`<p>${username}</p>`);
+    }
+  });
 
   //Output the new message
 socket.on('new message', (data) => {
